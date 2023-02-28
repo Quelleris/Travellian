1436d615566192ed3833388ff01d3abc5da66dd2
Quelleris <74406524+Quelleris@users.noreply.github.com>
2023-02-28 12:35:42 +0100

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const navTogglers = document.querySelectorAll('[data-nav-toggler]')
+const navbar = document.querySelector('[data-navbar]')
+const header = document.querySelector('[data-header]')
+const logoImg = document.querySelector('.logo-img')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active')
+        logoImg.src="src/images/Logo-dark.svg"
+        
+    } else {
+        header.classList.remove('active')
+        logoImg.src="src/images/Logo-light.svg"
+    }
+})
+
+navTogglers.forEach(elem => {
+    elem.addEventListener('click', toggleNav)
+})
+
+function toggleNav(event) {
+    navbar.classList.toggle('active')
+    document.body.classList.toggle('nav-open')
+    console.log(event)
+}
